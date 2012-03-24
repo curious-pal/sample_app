25975d1778fdf1083f207e00178de6f3dd6e168c
curious-pal <curious-pal@yandex.ru>
2012-03-24 22:26:27 +0400

    css, new README

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Return a title on a per-page basis.
+  def title												# Определение метода
+    base_title = "Ruby on Rails Tutorial Sample App"	# Назначение переменной
+    if @title.nil?										# проверка (bolean) на nil
+      base_title										# неявный return
+    else
+      "#{base_title} | #{@title}"						# интерполяция попробовать переделать  на (base_title + " | " + @title)
+    end
+  end
 end
